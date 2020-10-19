4ea301bcb9ec4a4068c80ab7844ac178ffc93d56
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-10-19 11:15:26 +0200

    record

---
+++ MusicRecords/record.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecords
+{
+    public class record
+    {
+        private string _title;
+        private string _artist;
+        private int _duration;
+        private string _yearOfPublication;
+
+        public record()
+        {
+            
+        }
+
+        public record(string title, string artist, int duration, string yearOfPublication)
+        {
+            _title = title;
+            _artist = artist;
+            _duration = duration;
+            _yearOfPublication = yearOfPublication;
+        }
+
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public string YearOfPublication { get; set; }
+    }
+}
