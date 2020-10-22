e55d37fff5cdf60b1e1eae8bb8ac9c33e2a70cae
Sebastian Halkjær Petersen <seba449h@edu.easj.dk>
2020-10-22 12:58:43 +0200

    Added Swagger

--- MusicRecords/Startup.cs
+++ MusicRecords/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MusicRecords
 {
@@ -25,6 +26,10 @@ namespace MusicRecords
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Items API", Version = "v1.0"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +48,9 @@ namespace MusicRecords
             {
                 endpoints.MapControllers();
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Records API v1.0"));
+
         }
     }
 }
